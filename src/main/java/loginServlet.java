94b104575c1da33f797e9bf34583db2866c8d696
Richn14 <richardnguyen1224@hotmail.com>
2022-03-16 19:37:17 -0400

    Update loginServlet.java

--- src/main/java/loginServlet.java
+++ src/main/java/loginServlet.java
@@ -4,7 +4,7 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
 
-@WebServlet(name = "loginServlet",value = "/login-servlet")
+
 public class loginServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
