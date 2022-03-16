2f9c0d14d17d7acd33cdb0a5044eb6684beaa8df
Richn14 <richardnguyen1224@hotmail.com>
2022-03-16 19:37:40 -0400

    Update logoutServlet.java

--- src/main/java/logoutServlet.java
+++ src/main/java/logoutServlet.java
@@ -2,7 +2,7 @@ import java.io.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
-@WebServlet(name = "logoutServlet",value = "/logout-servlet")
+
 public class logoutServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
